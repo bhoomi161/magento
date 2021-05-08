6327ef53e14323777504527e24bee32667bd4b4c
bhoomi161 <bhoomimehta161@gmail.com>
2021-05-08 17:48:45 +0530

    eav setup added

---
+++ Ccc/Competitor/Block/Adminhtml/Competitor/Attribute/Set/Main/Tree/Attribute.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Ccc_Competitor_Block_Adminhtml_Competitor_Set_Main_Tree_Attribute extends Mage_Adminhtml_Block_Template
+{
+    protected function _construct()
+    {
+        $this->setTemplate('catalog/product/attribute/set/main/tree/attribute.phtml');
+    }
+}
---
+++ Ccc/Competitor/Block/Adminhtml/Competitor/Edit/Tabs.php
@@ -0,0 +1,90 @@
+<?php
+
+class Ccc_Competitor_Block_Adminhtml_Competitor_Edit_Tabs extends Mage_Adminhtml_Block_Widget_Tabs
+{
+
+    public function __construct()
+    {
+      parent::__construct();
+      $this->setDestElementId('edit_form');
+      $this->setTitle(Mage::helper('competitor')->__('Competitor Information'));
+    }
+    
+    public function getCompetitor()
+    {
+        if (!($this->getData('competitor') instanceof Ccc_Competitor_Model_Competitor)) {
+            $this->setData('competitor', Mage::registry('current_competitor'));
+        }
+        return $this->getData('competitor');
+    }
+
+    protected function _beforeToHtml()
+    {
+
+        $competitor= $this->getCompetitor();
+
+        if (!($setId = $competitor->getAttributeSetId())) {
+            $setId = $this->getRequest()->getParam('set', null);
+        }
+        if($setId){
+        
+        $groupCollection = Mage::getResourceModel('eav/entity_attribute_group_collection')
+            ->setAttributeSetFilter($setId)
+            ->setSortOrder()
+            ->load();
+
+        $competitorAttributes = Mage::getResourceModel('competitor/competitor_attribute_collection');
+
+        if (!$this->getCompetitor()->getId()) {
+            foreach ($competitorAttributes as $attribute) {
+                $default = $attribute->getDefaultValue();
+                if ($default != '') {
+                    $this->getCompetitor()->setData($attribute->getAttributeCode(), $default);
+                }
+            }
+        }
+
+        $defaultGroupId = 0;
+        foreach ($groupCollection as $group) {
+            if ($defaultGroupId == 0 or $group->getIsDefault()) {
+                $defaultGroupId = $group->getId();
+            }
+
+        }	
+
+        foreach ($groupCollection as $group) {
+            $attributes = array();
+            foreach ($competitorAttributes as $attribute) {
+                if ($this->getCompetitor()->checkInGroup($attribute->getId(),$setId, $group->getId())) {
+                    $attributes[] = $attribute;
+                }
+            }
+
+            if (!$attributes) {
+                continue;
+            }
+
+            $active = $defaultGroupId == $group->getId();
+            $block = $this->getLayout()->createBlock('competitor/adminhtml_competitor_edit_tab_attributes')
+                ->setGroup($group)
+                ->setAttributes($attributes)
+                ->setAddHiddenFields($active)
+                ->toHtml();
+            $this->addTab('group_' . $group->getId(), array(
+                'label'     => Mage::helper('competitor')->__($group->getAttributeGroupName()),
+                'content'   => $block,
+                'active'    => $active
+            ));
+        }
+    }
+    else{
+        $this->addTab('set', array(
+            'label'     => Mage::helper('competitor')->__('Settings'),
+            'content'   => $this->getLayout()
+                ->createBlock('competitor/Adminhtml_competitor_edit_tab_settings')->toHtml(),
+            'active'    => true
+        ));
+    }
+      return parent::_beforeToHtml();
+    }
+}

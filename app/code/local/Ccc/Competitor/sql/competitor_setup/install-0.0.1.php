4ed1d2e9efe9d7b1b8fee87f3d6369226036167d
bhoomi161 <bhoomimehta161@gmail.com>
2021-05-11 19:43:13 +0530

    changes done

--- app/code/local/Ccc/Competitor/sql/competitor_setup/install-0.0.1.php
+++ app/code/local/Ccc/Competitor/sql/competitor_setup/install-0.0.1.php
@@ -9,8 +9,8 @@ $installer->addEntityType(Ccc_Competitor_Model_Resource_Competitor::ENTITY,array
     'attribute_model'=>'competitor/attribute',
     'table'=>'competitor/competitor',
     'increment_per_store'=>'0',
-    'addtional_attribute_table'=>'competitor/eav_attribute',
-    'entity_attribute_collection'=>'_collection',
+    'additional_attribute_table'=>'competitor/eav_attribute',
+    'entity_attribute_collection'=>'competitor/competitor_attribute_collection',
 ));
 
 $this->createEntityTables('competitor');
